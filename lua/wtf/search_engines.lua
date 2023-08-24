f458708b5906e83ac846f24e3b2f22a2f1d4402c
Piers Olenski <hello@piers.io>
2023-08-24 23:40:15 +0100

    Get sources from module table

--- lua/wtf/search_engines.lua
+++ lua/wtf/search_engines.lua
@@ -1,20 +1,15 @@
 local M = {}
 
 M.sources = {
-  google = "https://www.google.com/search?q=",
   duck_duck_go = "https://duckduckgo.com/?q=",
-  stack_overflow = "https://stackoverflow.com/search?q=",
   github = "https://github.com/search?type=issues&q=",
+  google = "https://www.google.com/search?q=",
+  stack_overflow = "https://stackoverflow.com/search?q=",
 }
 
 function M.get_completions()
   local list = {}
-  for key, _ in pairs({
-    google = "https://www.google.com/search?q=",
-    duck_duck_go = "https://duckduckgo.com/?q=",
-    stack_overflow = "https://stackoverflow.com/search?q=",
-    github = "https://github.com/search?type=issues&q=",
-  }) do
+  for key, _ in pairs(M.sources) do
     table.insert(list, key)
   end
   return list
