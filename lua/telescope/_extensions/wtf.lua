62971d29180ff74786f2efa952a7660586913997
Piers Olenski <hello@piers.io>
2024-08-27 20:15:25 +0100

    Remove unneccesary code

--- lua/telescope/_extensions/wtf.lua
+++ lua/telescope/_extensions/wtf.lua
@@ -1,76 +1,13 @@
-local has_telescope, telescope = pcall(require, "telescope")
+local config = require("wtf.config")
+local telescope = require("telescope")
+local builtin = require("telescope.builtin")
 
-if not has_telescope then
-  error("This plugin requires nvim-telescope/telescope.nvim")
-end
-
-local create_summary = require("wtf.utils.create_summary")
-local Path = require("plenary.path")
-local telescope_config = require("telescope.config").values
-local wtf_config = require("wtf.config")
-local finders = require("telescope.finders")
-local pickers = require("telescope.pickers")
-local previewers = require("telescope.previewers")
-
--- Retrieve the first line from each markdown file in the directory
--- Store the file paths for previewing
-local function get_chat_summaries(dir)
-  local file_paths = vim.fn.globpath(dir, "*.md", false, true)
-  local lines = {}
-  local files_with_first_line = {}
-  for _, path in ipairs(file_paths) do
-    local file = io.open(path, "r")
-    if file then
-      local first_line = file:read("*l")
-      local summary_length = 50
-
-      if first_line then
-        local summary = create_summary(first_line, summary_length)
-        table.insert(lines, summary)
-        table.insert(files_with_first_line, { first_line = summary, path = path })
-      end
-      file:close()
-    end
-  end
-  return files_with_first_line
-end
-
-local history = function(opts)
-  opts = opts or {}
-
-  local chat_dir = wtf_config.options.chat_dir
-  local summaries = get_chat_summaries(chat_dir)
-
-  local markdown_content = previewers.new_buffer_previewer({
-    define_preview = function(self, entry)
-      if not entry or not entry.path then
-        return
-      end
-      local p = Path:new(entry.path)
-      local content = p:readlines()
-      vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, content)
-      vim.bo[self.state.bufnr].filetype = "markdown"
-    end,
-  })
-
-  pickers
-    .new(opts, {
-      prompt_title = "WTF History",
-      finder = finders.new_table({
-        results = summaries,
-        entry_maker = function(entry)
-          return {
-            value = entry,
-            display = entry.first_line,
-            ordinal = entry.first_line,
-            path = entry.path,
-          }
-        end,
-      }),
-      sorter = telescope_config.generic_sorter(opts),
-      previewer = markdown_content,
-    })
-    :find()
+local history = function()
+  local opts = {
+    cwd = config.options.chat_dir,
+    prompt_title = "WTF: Grep History",
+  }
+  builtin.live_grep(opts)
 end
 
 return telescope.register_extension({ exports = { history = history } })
