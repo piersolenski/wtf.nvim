13877a44a23c88c4f9c9e80b61d01d95244c8a8a
Piers Olenski <hello@piers.io>
2025-07-05 19:27:30 +0100

    Update to latest api

--- lua/wtf/util/get_programming_language.lua
+++ lua/wtf/util/get_programming_language.lua
@@ -1,6 +1,6 @@
 local function get_programming_language()
   local bufnr = vim.api.nvim_get_current_buf()
-  local filetype = vim.api.nvim_buf_get_option(bufnr, "filetype")
+  local filetype = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
 
   if filetype == "cpp" then
     return "C++"
